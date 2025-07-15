513ff4956ee70fc28fab3e744a57f1d3be876322
Daniel <daniel.corbellini28@gmail.com>
2025-07-14 21:02:33 -0300

    build: setting up the project

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,23 +1,9 @@
-import Counter from "../components/Counter";
-import Navigation from "../components/Navigation";
+import Counter from "@/app/_components/Counter";
 
-// Server component: Fetching data and using it in html with react components
-// We cannot use hooks in react server components
-// We can call components that uses hooks, see the Counter component
 export default async function Page() {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users");
-  const data = await res.json();
-
   return (
     <div>
       <h1>Cabins page</h1>
-      <ul>
-        {data.map((user) => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ul>
-
-      <Counter />
     </div>
   );
 }
