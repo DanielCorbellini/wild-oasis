a851ef4e59dd65e828a709d6f7fc8144b01849a8
Daniel <daniel.corbellini28@gmail.com>
2025-07-14 21:26:35 -0300

    style: creates components and some css

--- app/layout.js
+++ app/layout.js
@@ -1,22 +1,29 @@
-import Logo from "./_components/Logo";
-import Navigation from "./_components/Navigation";
-
+import Header from "./_components/Header";
 import "@/app/_styles/globals.css";
+import { Josefin_Sans } from "next/font/google";
+
+// Uses google fonts in the application
+const font = Josefin_Sans({ subsets: ["latin"], display: "swap" });
 
 export const metadata = {
-  title: "The Wild Oasis",
+  title: {
+    template: "%s / The Wild Oasis",
+    default: "Welcome / The Wild Oasis",
+  },
+  description:
+    "Luxurious cabin hotel, located in the heart of the Italian Dolomites, surrounded by the mountains and dark forest",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-primary-950 text-primary-100 min-h-screen">
-        <header>
-          <Logo />
-        </header>
-        <Navigation />
-        <main>{children}</main>
-        <footer>Copyright by The Wild Oasis</footer>
+      <body
+        className={`${font.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col`}
+      >
+        <Header />
+        <div className="flex-1 px-8 py-12">
+          <main className="max-w-7xl mx-auto">{children}</main>
+        </div>
       </body>
     </html>
   );
